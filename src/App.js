532bca214dbc09b5c67a78338e862dd23bf00a58
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-08-02 10:41:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Video from "./components/Video";
+
+function App(){
+  return  (
+    <>
+    <h1>Hello react!!</h1>
+     <Video></Video>
+
+    </>
+  );
+
+}
+
+export default App;
---
+++ src/components/Video.js
@@ -0,0 +1,11 @@
+import './video.css'
+function Video(){
+    return( 
+        <div>
+            <img className="Img" src="https://picsum.photos/seed/picsum/400/300" alt="Error"></img>
+            <h2>Hey There!! This is video component!</h2>
+        </div>
+    )
+}
+
+export default Video;
