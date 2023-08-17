6630dc312d1030c971118e9fb09c8cc187bc450d
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-08-17 22:27:26 +0530

    props learn

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,9 +1,10 @@
 import './video.css'
-function Video(){
+// props are always being in form of objects...
+function Video(porps){
     return( 
         <div>
             <img className="Img" src="https://picsum.photos/seed/picsum/400/300" alt="Error"></img>
-            <h2>Hey There!! This is video component!</h2>
+            <h2>`Hey There!! This is {porps.title} video component!`</h2>
         </div>
     )
 }
