1e44cf659166a033384b10b25cdd3b0712774ccd
Nagendra07-moh <nagedra.mohan.5@gmail.com>
2023-08-18 16:08:11 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css'
 function App(){
   return  (
     <>
-    <h1>Hello react!!</h1>
-     <Video title = "This is First" color ="red" ></Video>
-     <Video title= "This is second" color ="green" ></Video>
-     <Video title = "This is third" color ="blue" ></Video>
+     <Video title = "This is First" channel="PG-1" views="10k"  time="13:34 AM" verified ={true}></Video>
+     <Video title= "This is second" channel="PG-1" views="100k"  time="13:34 PM" verified = {false}></Video>
+     <Video title = "This is third" channel="PG-1" views="1200k"  time="13:34 PM" verified = {true}></Video>
+     <Video title = "This is third" channel="PG-1" views="1200k"  time="13:34 PM" verified = {false}></Video>
+     <Video title = "This is third" channel="PG-1" views="1200k"  time="13:34 PM" verified = {true}></Video>
+     <Video title = "This is third" channel="PG-1" views="1200k"  time="13:34 PM" verified = {false}></Video>
      </>
+
+
+
   );
 
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -3,15 +3,22 @@ import './video.css'
 //ABOUT PROPS=>
 // props are always being in form of objects...
 // These days we use props in  a destructured way
-function Video({title,channel,views,time}){
+function Video({title,channel,views,time,verified}){
+
     return( 
         <div className='cointaner'>
             <div className='pic'>
             <img className="Img" src="https://picsum.photos/seed/picsum/400/300" alt="Error"></img>
             </div>
             <div className='title'>{title}</div>
-            <div className='channel'>{channel}</div>
-            <div className='views'>{views} views <span>.</span> {time}</div>
+            {/* {channelJsx} */}
+
+            {/* //this is the best practice for using the conditional rendering  */}
+            {/* {verified ? <div className='channel'>{channel} {verified ? ✅ :null}</div> :<div className='channel'>{channel}</div>}   */}
+
+             <div className='channel'>{channel} {verified ? "✅" :null}</div>  
+
+            <div className='views'>{views} views.<span></span> {time}</div>
 
             
         </div>
